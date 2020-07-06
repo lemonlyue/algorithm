05fa112cb921e279a716d16b245a44e3bf75053a
lemon_lyue <lemon_lyue@163.com>
2020-07-06 21:02:25 +0800

    update: 添加1349代码

---
+++ source/LeetCode_1349.php
@@ -0,0 +1,31 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String[][] $paths
+     * @return String
+     */
+    function destCity($paths) {
+        $start = [];
+        $end = [];
+        // 拆分为两个数组,获取起点和终点
+        foreach ($paths as $item) {
+            $start[] = $item[0];
+            $end[] = $item[1];
+        }
+
+        /**
+         * @desc PHP内置获取数组某一列
+         */
+        // $start = array_column($paths, 0);
+        // $end = array_column($paths, 1);
+
+        foreach ($end as $item) {
+            // 判断终点是否存在于起点,存在则不是最终的终点
+            if (!in_array($item, $start)) {
+                return $item;
+            }
+        }
+    }
+}
