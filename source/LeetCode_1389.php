76ddd9fae47e826df623683517e58dcdbba2bfca
lemon_lyue <lemon_lyue@163.com>
2020-07-08 23:38:52 +0800

    update: 添加1389另一个解法

--- source/LeetCode_1389.php
+++ source/LeetCode_1389.php
@@ -29,4 +29,49 @@ class Solution {
             $this->$arr[$index] = $num;
         }
     }
+
+
+    /**
+     * @desc 将 index 数组中的每一项遍历，判断在插入后实际对应的下标位置得到 ($_tmp_index), 然后通过该映射写入到结果中。
+     * @param Integer[] $nums
+     * @param Integer[] $index
+     * @return Integer[]
+     */
+    function createTargetArray2($nums, $index) {
+
+        $target = [];
+
+        $_tmp_indexs = [];
+
+        foreach ($index as $i => $k) {
+
+            if (in_array($k, $_tmp_indexs)) {
+
+                foreach ($_tmp_indexs as &$_tmp_index) {
+
+                    if ($_tmp_index >= $k) {
+
+                        $_tmp_index ++;
+
+                    }
+
+                }
+
+            }
+
+            $_tmp_indexs[] = $k;
+
+        }
+
+        foreach ($_tmp_indexs as $i => $k) {
+
+            $target[$k] = $nums[$i];
+
+        }
+
+        ksort($target);
+
+        return $target;
+
+    }
 }
